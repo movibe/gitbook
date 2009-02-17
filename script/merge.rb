b0b617eb5f3086f9b31bee3c483304e033a2f070
Enderson Maia <endersonmaia@gmail.com>
2009-02-16 23:26:53 -0300

    * Fixes merge for UTF-8 file.

--- script/merge.rb
+++ script/merge.rb
@@ -3,7 +3,11 @@ desc 'Merge all of the texttile output into a single file for pdf conversion'
 task :merge do
   File.open('output/full_book.markdown', 'w+') do |f|
     Dir["text/**/*.markdown"].sort.each do |path|
-      f << File.new(path).read + "\r\n"
+      m = File.new(path)
+      m.readlines.each do |line|
+        f << line.gsub(/^\357\273\277/,'')
+      end
+      f << "\n"
     end
   end
 end
